dccd5adb59d98ae9d40c117288f39040668bb508
Muhammad Osama <imuhammadosama@outlook.com>
2020-06-29 20:35:47 +0500

    Connected to MongoDB.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 //ROUTES
 app.get('/', (req, res) => {
   res.send('This is the api home directory!');
+});
+
+//FIXING MONGOOSE ERRORS
+mongoose.set('useNewUrlParser', 'true');
+mongoose.set('useUnifiedTopology', 'true');
+
+//CONNECTING TO DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log('Connected to DB!');
 });
 
 //SERVER IS LISTENING
