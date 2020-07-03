3ec849d67ab801daace1e67098391f73ebc9ce09
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-03 11:07:23 +0500

    Client has been added.

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Client from './client.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Client />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
