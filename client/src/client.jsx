a8a4d08b61eb6858b001ff944654aaf33b9dc6c4
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-03 11:16:07 +0500

    Bootstrap added.

--- client/src/client.jsx
+++ client/src/client.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Client() {
-  return <div>Home</div>;
+  return (
+    <div className='container'>
+      <div className='row my-5'>
+        <h1 className='logo-text'>Blog</h1>
+      </div>
+      <div className='row my-5'>Content</div>
+    </div>
+  );
 }
